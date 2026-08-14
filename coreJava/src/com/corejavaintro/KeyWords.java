d7c8acaddb0146a06a7bc0eda2c990b78db1d93b
Chandu-Pusa <pusasrujan@gmail.com>
2026-08-14 16:52:18 +0530

    KeyWords in Java

---
+++ coreJava/src/com/corejavaintro/KeyWords.java
@@ -0,0 +1,121 @@
+package com.corejavaintro;
+
+public class KeyWords {
+
+    /*
+     * Java keywords are reserved words that have predefined meanings in the
+     * Java language.
+     *
+     * They cannot be used as identifiers (such as variable names, class names,
+     * or method names).
+     *
+     * For easier understanding, they are categorized into 6 types
+     *
+     * I) Primitive Data Types
+     *
+     * 1. byte
+     * 2. short
+     * 3. int
+     * 4. long
+     * 5. float
+     * 6. double
+     * 7. char
+     * 8. boolean
+     *
+     *
+     * II) Application Level & OOP
+     *
+     * a) Top Level
+     *
+     * 9.  package
+     * 10. import
+     *
+     * b) Type / Class Level
+     *
+     * 11. class
+     * 12. interface
+     * 13. enum
+     * 14. new
+     * 15. instanceof
+     *
+     * c) Method Level
+     *
+     * 16. void
+     * 17. return
+     * 18. var
+     *
+     *
+     * d) OOP Level
+     *
+     * 19. this
+     * 20. super
+     * 21. extends
+     * 22. implements
+     * 23. assert
+     *
+     *
+     * III) Exception Handling
+     *
+     * 24. try
+     * 25. catch
+     * 26. finally
+     * 27. throw
+     * 28. throws
+     * 29. try()
+     *
+     *
+     * IV) Access Modifiers
+     *
+     * 30. private
+     * 31. default     
+     * 32. protected
+     * 33. public
+     *
+     *
+     * V) Non-Access Modifiers
+     *
+     * 34. abstract
+     * 35. static
+     * 36. strictfp
+     * 37. synchronized
+     * 38. transient
+     * 39. volatile
+     * 40. native
+     * 41. final
+     *
+     *
+     * VI) Logical & Conditional / Loop Statements
+     *
+     * 42. if
+     * 43. else
+     * 44. switch
+     * 45. case
+     * 46. break
+     * 47. default
+     * 48. yield
+     * 49. continue
+     * 50. for
+     * 51. while
+     * 52. do
+     *
+     *
+     * ** Reserved but not used **
+     *
+     * 53. goto
+     * 54. const
+     *
+     *
+     * IMPORTANT NOTES:
+     *
+     * 1. var is a reserved type name introduced in Java 10.
+     * 2. default is NOT an access modifier keyword.
+     *    "Default access" means package-private access when no
+     *    access modifier is specified.
+     * 3. try() is NOT a separate keyword. It is the syntax used
+     *    for try-with-resources.
+     * 4. instanceof is an operator/keyword.
+     * 5. goto and const are reserved but not used in Java.
+     *
+     */
+
+}
