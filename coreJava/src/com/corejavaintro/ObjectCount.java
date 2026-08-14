1c829d9eecf552249143774949d15d6a10293c8f
Chandu-Pusa <pusasrujan@gmail.com>
2026-08-14 12:03:04 +0530

    updated gitignore

--- coreJava/src/com/corejavaintro/ObjectCount.java
+++ coreJava/src/com/corejavaintro/ObjectCount.java
@@ -4,22 +4,28 @@ public class ObjectCount {
 	
 	static int count = 0;
 	
-	{
-		count += 1;
+    {
+		count ++;
 	}
 	
 	public static void main(String[] args) {
 		
+		
 		@SuppressWarnings("unused")
 		ObjectCount obj1 = new ObjectCount();
+		
 		@SuppressWarnings("unused")
 		ObjectCount obj2 = new ObjectCount();
+		
 		@SuppressWarnings("unused")
 		ObjectCount obj3 = new ObjectCount();
+		
 		@SuppressWarnings("unused")
 		ObjectCount obj4 = new ObjectCount();
+		
 		@SuppressWarnings("unused")
 		ObjectCount obj5 = new ObjectCount();
+		
 		System.out.println(count);
 	}
 }
