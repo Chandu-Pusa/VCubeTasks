796ecf94e66dc768ffb3f8401ab7fb4b7a9633ec
Chandu-Pusa <pusasrujan@gmail.com>
2026-09-12 13:37:07 +0530

    commit

--- coreJava/src/com/conditional/Calculator.java
+++ coreJava/src/com/conditional/Calculator.java
@@ -5,58 +5,82 @@ import java.util.Scanner;
 public class Calculator {
 
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter first Number: ");
-		int n1 = sc.nextInt();
-		System.out.println("Enter the Second Number: ");
-		int n2 = sc.nextInt();
-		System.out.println("Enter one of the Operations + - * /");
-		String opr = sc.next();
+		String choice;
 
-		switch (opr) {
-		case "+": {
-			System.out.println();
-			System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 + n2));
-			break;
-		}
-		case "-": {
-			System.out.println();
-			System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 - n2));
-			break;
-		}
-		case "*": {
-			System.out.println();
-			System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 * n2));
-			break;
-		}
-		case "/": {
-			System.out.println();
-			if (n2 == 0) {
-				System.out.println("We Cannot perform division by Zero");
-				break;
-			} else {
-				System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 / n2));
+		do {
+
+			System.out.println("Enter first Number: ");
+			int n1 = sc.nextInt();
+
+			System.out.println("Enter the Second Number: ");
+			int n2 = sc.nextInt();
+
+			System.out.println("Enter one of the Operations + - * / %");
+			String opr = sc.next();
+
+			switch (opr) {
+
+			case "+": {
+				System.out.println();
+				System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 + n2));
 				break;
 			}
-		}
-		case "%": {
-			System.out.println();
-			if (n2 == 0) {
-				System.out.println("We Cannot perform modulo by Zero");
-				break;
-			} else {
-				System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 % n2));
+
+			case "-": {
+				System.out.println();
+				System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 - n2));
 				break;
 			}
-		}
-		default: {
+
+			case "*": {
+				System.out.println();
+				System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 * n2));
+				break;
+			}
+
+			case "/": {
+				System.out.println();
+
+				if (n2 == 0) {
+					System.out.println("We Cannot perform division by Zero");
+					break;
+				} else {
+					System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 / n2));
+					break;
+				}
+			}
+
+			case "%": {
+				System.out.println();
+
+				if (n2 == 0) {
+					System.out.println("We Cannot perform modulo by Zero");
+					break;
+				} else {
+					System.out.println(n1 + " " + opr + " " + n2 + " = " + (n1 % n2));
+					break;
+				}
+			}
+
+			default: {
+				System.out.println();
+				System.out.println("Enter valid operator");
+				break;
+			}
+
+			}
+
 			System.out.println();
-			System.out.println("Enter valid operator");
-			break;
-		}
-		}
+			System.out.println("Do you want to perform another calculation? (y/n)");
+			choice = sc.next();
+
+		} while (choice.equalsIgnoreCase("y"));
+
+		System.out.println("Calculator closed.");
+
 		sc.close();
 	}
-
 }
